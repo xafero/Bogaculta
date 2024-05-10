863d7a2f9236538fdefa632edbd5742e651aed37
xafero <xafero@users.noreply.github.com>
2024-05-10 20:41:17 +0200

    Adding initial code

---
+++ bogaculta/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"image/color"
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		window := new(app.Window)
+		err := run(window)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+func run(window *app.Window) error {
+	theme := material.NewTheme()
+	var ops op.Ops
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+
+			title := material.H1(theme, "Hello, Gio")
+
+			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+			title.Color = maroon
+
+			title.Alignment = text.Middle
+
+			title.Layout(gtx)
+
+			e.Frame(gtx.Ops)
+		}
+	}
+}
